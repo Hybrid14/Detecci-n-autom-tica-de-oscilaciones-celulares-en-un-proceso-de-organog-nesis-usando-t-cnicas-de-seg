e417279176be4b9e183ee644202b7505743f2adc
Luis Guzmán <l_pipe_@hotmail.com>
2020-12-24 00:33:08 -0300

    Z fuera de la función, se prueba con z=13

--- pytorch-unet-segmentation-master/scripts/CreatePNG.py
+++ pytorch-unet-segmentation-master/scripts/CreatePNG.py
@@ -1,8 +1,9 @@
 from os.path import join
 import matplotlib.pyplot as plt
-def png(path,image_arrays):
+def png(path,image_arrays,z):
     for x in range (0, 60):
-        img = image_arrays[0, 0, 0, x, 0, :, :, 0]
+                            #[0,0,0,t,z,x,y,0]
+        img = image_arrays[0, 0, 0, x, z, :, :, 0]
         if(x<10):
             plt.imsave(join(path, "0"+str(x)+".png"), img, cmap='gray_r') #guarda en escala de grises invertido
         else:
